2146a4f2684e46752c9e8840e1a8a9ed90fd739b
david-swift <haenerdavid@icloud.com>
2024-03-17 21:30:53 +0100

    Set whether a label is ellipsized

--- Sources/Adwaita/View/Text.swift
+++ Sources/Adwaita/View/Text.swift
@@ -5,6 +5,8 @@
 //  Created by david-swift on 23.08.23.
 //
 
+import CAdw
+
 /// A text widget.
 public typealias Text = Label
 
@@ -14,6 +16,13 @@ extension Text {
     /// - Parameter text: The content.
     public init(_ text: String) {
         self.init(label: text)
+    }
+
+    /// Set whether the text should ellipsize at the end.
+    /// - Parameter ellipsize: Whether it should ellipsize.
+    /// - Returns: The text widget.
+    public func ellipsize(_ ellipsize: Bool = true) -> View {
+        inspect { gtk_label_set_ellipsize($0.pointer, PANGO_ELLIPSIZE_END) }
     }
 
 }
